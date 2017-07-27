c9b7e5475b0b1e4f1c3ee6008f59909385a1ca7c
hanker <jinhui0106@126.com>
2017-07-27 11:38:58 +0800

    tablayout 实现 顶部导航

--- app/src/main/java/com/edu/base/BaseActivity.java
+++ app/src/main/java/com/edu/base/BaseActivity.java
@@ -1,7 +1,6 @@
 package com.edu.base;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,9 +12,10 @@ import android.support.v7.app.AppCompatActivity;
 
 public abstract  class BaseActivity extends AppCompatActivity {
 
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(getContentId());
 
         initView();
--- app/src/main/java/com/edu/ui/activity/HomeActivity.java
+++ app/src/main/java/com/edu/ui/activity/HomeActivity.java
@@ -3,15 +3,22 @@ package com.edu.ui.activity;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.edu.base.BaseActivity;
 import com.edu.base.BaseFragment;
 import com.edu.mycourse.R;
+import com.edu.ui.adapter.MyFragmentAdapter;
 import com.edu.ui.fragment.CourseFragment;
 import com.edu.ui.fragment.FindFragment;
 import com.edu.ui.fragment.MineFragment;
 
 import java.util.ArrayList;
+
+import static com.edu.mycourse.R.id.tabLayout;
 
 /**
  * @ 作者 J.H
@@ -28,7 +35,16 @@ public class HomeActivity extends BaseActivity {
     private BaseFragment mFindFragment;
     private BaseFragment mCourseFragment;
     private BaseFragment mMineFragment;
+    private MyFragmentAdapter mAdapter;
 
+    //Tab 文字
+    private final int[] TAB_TITLES =
+            new int[]{R.string.tab_find,R.string.tab_course,R.string.tab_mine};
+
+    //Tab 图片
+    private final int[] TAB_IMGS =
+            new int[]{R.drawable.tab_find_selector,
+                    R.drawable.tab_course_selector,R.drawable.tab_mine_selector};
 
 
 
@@ -40,11 +56,13 @@ public class HomeActivity extends BaseActivity {
     @Override
     public void initView() {
         mViewPager = (ViewPager) findViewById(R.id.viewpager);
-        mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        mTabLayout = (TabLayout) findViewById(tabLayout);
+
     }
 
     @Override
     public void initData() {
+
         mFragments = new ArrayList<>();
         mFindFragment = new FindFragment();
         mCourseFragment = new CourseFragment();
@@ -52,12 +70,49 @@ public class HomeActivity extends BaseActivity {
         mFragments.add(mFindFragment);
         mFragments.add(mCourseFragment);
         mFragments.add(mMineFragment);
+        mAdapter = new MyFragmentAdapter(getSupportFragmentManager(), mFragments);
+        mViewPager.setAdapter(mAdapter);
 
+        mTabLayout.setupWithViewPager(mViewPager);
+        initTab();
+//        setTabs(mTabLayout, this.getLayoutInflater(), TAB_TITLES, TAB_IMGS);
 
     }
 
+    private void initTab() {
+        mTabLayout.getTabAt(0).setCustomView(R.layout.tab_find_item);
+        mTabLayout.getTabAt(1).setCustomView(R.layout.tab_course_item);
+        mTabLayout.getTabAt(2).setCustomView(R.layout.tab_mine_item);
+
+        //默认选中的Tab
+        mTabLayout.getTabAt(0).getCustomView().setSelected(true);
+
+    }
+
+    /**
+     * @description: 设置添加Tab
+     */
+    private void setTabs(TabLayout tabLayout, LayoutInflater inflater, int[] tabTitles, int[] tabImgs){
+        for (int i = 0; i < tabImgs.length; i++) {
+            TabLayout.Tab tab = tabLayout.newTab();
+            View view = inflater.inflate(R.layout.tab_find_item,null);
+            tab.setCustomView(view);
+
+            TextView tvTitle = (TextView)view.findViewById(R.id.tv_tab);
+            tvTitle.setText(tabTitles[i]);
+            ImageView imgTab = (ImageView) view.findViewById(R.id.img_tab);
+            imgTab.setImageResource(tabImgs[i]);
+            tabLayout.addTab(tab);
+
+        }
+    }
+
+
     @Override
     public void setListener() {
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+        mTabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
 
     }
 }
