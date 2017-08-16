10e62d0ba3cc023476f570b5478962cd3c61821e
hanker <jinhui0106@126.com>
2017-08-16 16:09:10 +0800

    首页布局添加

--- app/src/main/java/com/edu/ui/fragment/FindFragment.java
+++ app/src/main/java/com/edu/ui/fragment/FindFragment.java
@@ -1,6 +1,8 @@
 package com.edu.ui.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.View;
 
 import com.edu.base.BaseFragment;
@@ -12,7 +14,10 @@ import com.edu.mycourse.R;
  * Created by J.H on 2017/7/27.
  */
 
-public class FindFragment extends BaseFragment {
+public class FindFragment extends BaseFragment implements  SwipeRefreshLayout.OnRefreshListener{
+
+
+    SwipeRefreshLayout mSRLayout;
 
 
 
@@ -23,11 +28,25 @@ public class FindFragment extends BaseFragment {
 
     @Override
     public void initView(View view, Bundle savedInstanceState) {
-
+        mSRLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefreshLayout);
     }
 
     @Override
     public void initData() {
+        //设置下拉出现小圆圈是否是缩放出现，出现的位置，最大的下拉位置
+//        mSRLayout.setProgressViewOffset(true, 50, 200);
+        //设置下拉圆圈的大小，两个值 LARGE， DEFAULT
+//        mSRLayout.setSize(SwipeRefreshLayout.LARGE);
+        mSRLayout.setColorSchemeColors(Color.BLUE,
+                Color.GREEN,
+                Color.YELLOW,
+                Color.RED);
+
+
+    }
+
+    @Override
+    public void onRefresh() {
 
     }
 }
