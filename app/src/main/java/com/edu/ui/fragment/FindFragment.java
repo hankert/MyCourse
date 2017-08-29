dd7f9c31249e5235f340e487b6444d6c8a3f9461
hanker <jinhui0106@126.com>
2017-08-29 10:04:45 +0800

    修改布局

--- app/src/main/java/com/edu/ui/fragment/FindFragment.java
+++ app/src/main/java/com/edu/ui/fragment/FindFragment.java
@@ -2,11 +2,20 @@ package com.edu.ui.fragment;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.bigkoo.convenientbanner.ConvenientBanner;
+import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
+import com.bigkoo.convenientbanner.listener.OnItemClickListener;
 import com.edu.base.BaseFragment;
 import com.edu.mycourse.R;
+import com.edu.ui.adapter.NetImageViewHolder;
+import com.edu.utils.ToastUtils;
+
+import java.util.ArrayList;
 
 /**
  * @auther jh
@@ -17,8 +26,10 @@ import com.edu.mycourse.R;
 public class FindFragment extends BaseFragment implements  SwipeRefreshLayout.OnRefreshListener{
 
 
-    SwipeRefreshLayout mSRLayout;
-
+    private SwipeRefreshLayout mSRLayout;
+    private ArrayList<String> imageUrls = null;
+    private ConvenientBanner convenientBanner;
+    private RecyclerView mRecycleView;
 
 
     @Override
@@ -29,6 +40,8 @@ public class FindFragment extends BaseFragment implements  SwipeRefreshLayout.On
     @Override
     public void initView(View view, Bundle savedInstanceState) {
         mSRLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefreshLayout);
+        convenientBanner = (ConvenientBanner) view.findViewById(R.id.convenientBanner);
+        mRecycleView = (RecyclerView) view.findViewById(R.id.recycleView);
     }
 
     @Override
@@ -42,8 +55,68 @@ public class FindFragment extends BaseFragment implements  SwipeRefreshLayout.On
                 Color.YELLOW,
                 Color.RED);
 
+        imageUrls = new ArrayList<>();
+        imageUrls.add("http://bank.wo.cn/public/actimage/20170320095415.jpg");
+        imageUrls.add("http://bank.wo.cn/public/actimage/20170117105807.png");
+        imageUrls.add("http://bank.wo.cn/public/actimage/20170317154211.png");
+        imageUrls.add("http://bank.wo.cn/public/actimage/20170317152743.jpg");
+
+        pullBanner();
 
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        convenientBanner.startTurning(3000);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        convenientBanner.stopTurning();
+    }
+
+    private void pullBanner() {
+        convenientBanner.setPages(new CBViewHolderCreator<NetImageViewHolder>() {
+            @Override
+            public NetImageViewHolder createHolder() {
+                return new NetImageViewHolder();
+            }
+        }, imageUrls)
+                .setPageIndicator(new int[]{R.drawable.ic_page_indicator, R.drawable.ic_page_indicator_focused})
+                .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.ALIGN_PARENT_RIGHT);
+
+        convenientBanner.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+//                ToastUtils.showLong(getActivity(), "您翻到了"+position+"页");
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+
+            }
+        });
+
+
+        convenientBanner.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                ToastUtils.showLong(getActivity(), "您点击了"+position+"页");
+
+            }
+        });
+
+
+    }
+
 
     @Override
     public void onRefresh() {
